cfe8facb6d7349bd547af72e8cf5248260b564b6
Lars Michaelis <mail@larmic.de>
2016-02-19 10:29:45 +0100

    registry: check if resource is already registered

--- registry/src/main/java/de/larmic/wildfly/swarm/serviceregistry/ApplicationResource.java
+++ registry/src/main/java/de/larmic/wildfly/swarm/serviceregistry/ApplicationResource.java
@@ -2,6 +2,9 @@ package de.larmic.wildfly.swarm.serviceregistry;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
@@ -11,9 +14,10 @@ import javax.ws.rs.core.Response;
 import java.util.Date;
 import java.util.List;
 
-@Path("/application")
+@Path(ApplicationResource.APPLICATION)
 public class ApplicationResource {
 
+    public static final String APPLICATION = "/application";
     @PersistenceContext
     private EntityManager em;
 
@@ -21,7 +25,8 @@ public class ApplicationResource {
     @Produces({"application/json", "application/xml"})
     public Response get() {
         final List<ApplicationEntity> applications = em.createNamedQuery("Application.findAll", ApplicationEntity.class).getResultList();
-        final GenericEntity<List<ApplicationEntity>> listGenericEntity = new GenericEntity<List<ApplicationEntity>>(applications) {};
+        final GenericEntity<List<ApplicationEntity>> listGenericEntity = new GenericEntity<List<ApplicationEntity>>(applications) {
+        };
         return Response.ok(listGenericEntity).build();
     }
 
@@ -29,22 +34,48 @@ public class ApplicationResource {
     @Path("/{name}")
     @Transactional
     public Response registerApplication(@PathParam("name") final String name, @Context HttpServletRequest request) {
-        // TODO check if application already exist
-        final ApplicationEntity application = createApplication(name, request);
-        em.persist(application);
+        ApplicationEntity application = findApplication(name, request.getRemoteHost(), request.getRemotePort());
+
+        if (application == null) {
+            application = createApplication(name, request);
+            em.persist(application);
+        }
+
         final String response = String.format("Application %s registered at %s", application.getName(), application.getRegistrationTime());
-        return Response.status(200).entity(response).build();
+        return Response.status(200).header("location", createApplicationHeaderLocation(application)).entity(response).build();
     }
 
-    private ApplicationEntity createApplication(final  String name, final HttpServletRequest request) {
+    private ApplicationEntity findApplication(final String name, final String serverName, final int port) {
+        final CriteriaBuilder cb = em.getCriteriaBuilder();
+        final CriteriaQuery<ApplicationEntity> query = cb.createQuery(ApplicationEntity.class);
+        final Root<ApplicationEntity> root = query.from(ApplicationEntity.class);
+        query.select(root)
+                .where(cb.and(
+                        cb.equal(root.get("name"), name),
+                        cb.equal(root.get("serverName"), serverName),
+                        cb.equal(root.get("port"), port)))
+                .orderBy(cb.asc(root.get("port")));
+
+        final List<ApplicationEntity> applications = em.createQuery(query).getResultList();
+        return applications.isEmpty() ? null : applications.get(0);
+    }
+
+    private String createApplicationHeaderLocation(final ApplicationEntity application) {
+        return String.format("%s%s/%s/%s/%d",
+                MyApplication.APPLICATION_PATH,
+                ApplicationResource.APPLICATION,
+                application.getName(),
+                application.getServerName(),
+                application.getPort());
+    }
+
+    private ApplicationEntity createApplication(final String name, final HttpServletRequest request) {
         final ApplicationEntity application = new ApplicationEntity();
         application.setName(name);
         application.setLastActive(new Date());
         application.setRegistrationTime(new Date());
-
-        // TODO set correct server name and port
-        application.setServerName(request.getServerName());
-        application.setPort(request.getLocalPort());
+        application.setServerName(request.getRemoteHost());
+        application.setPort(request.getRemotePort());
         return application;
     }
 }
--- registry/src/main/java/de/larmic/wildfly/swarm/serviceregistry/MyApplication.java
+++ registry/src/main/java/de/larmic/wildfly/swarm/serviceregistry/MyApplication.java
@@ -3,8 +3,10 @@ package de.larmic.wildfly.swarm.serviceregistry;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/registry")
+@ApplicationPath(MyApplication.APPLICATION_PATH)
 public class MyApplication extends Application {
+
+    public static final String APPLICATION_PATH = "/registry";
 
     public MyApplication() {
     }
